62dbb8db281372ac733f5bcf53fa8c9392681e5f
Michael Ajao-olarinoye <ajaoolarinoyemichael@gmail.com>
2024-01-08 13:37:48 +0000

    mew

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from IPython.display import display
 
 # --------------------------------------------------------------
 # Load data
@@ -10,22 +11,48 @@ df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 # --------------------------------------------------------------
 # Plot single columns
 # --------------------------------------------------------------
+set_df = df[df["set"]==1]
+plt.plot(set_df["acc_y"])
 
-
+plt.plot(set_df["acc_y"].reset_index(drop=True))
 # --------------------------------------------------------------
 # Plot all exercises
 # --------------------------------------------------------------
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    display(subset)
 
 
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset["acc_y"].reset_index(drop=True), label = label)
+    plt.legend()
+    plt.show()
+    
+
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label = label)
+    plt.legend()
+    plt.show()
 # --------------------------------------------------------------
 # Adjust plot settings
 # --------------------------------------------------------------
 
+mpl.style.use("seaborn-v0_8-paper")
+mpl.rcParams["figure.figsize"] = (20, 8)
+mpl.rcParams["figure.dpi"] = 100
 
 # --------------------------------------------------------------
 # Compare medium vs. heavy sets
 # --------------------------------------------------------------
 
+category_df = df.query("label == 'squat'").query("participant == 'A'").reset_index()
+
+
+category_df.groupby(["category"])["acc_y"].plot()
 
 # --------------------------------------------------------------
 # Compare participants
