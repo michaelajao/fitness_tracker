949f08f509784d20bcffa4ef14d157ea255f898b
michaelajao <ajaoolarinoyemichael@gmail.com>
2024-01-08 15:43:44 +0000

    update data and visualisation continue

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -11,7 +11,7 @@ df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 # --------------------------------------------------------------
 # Plot single columns
 # --------------------------------------------------------------
-set_df = df[df["set"]==1]
+set_df = df[df["set"] == 1]
 plt.plot(set_df["acc_y"])
 
 plt.plot(set_df["acc_y"].reset_index(drop=True))
@@ -26,15 +26,15 @@ for label in df["label"].unique():
 for label in df["label"].unique():
     subset = df[df["label"] == label]
     fig, ax = plt.subplots()
-    plt.plot(subset["acc_y"].reset_index(drop=True), label = label)
+    plt.plot(subset["acc_y"].reset_index(drop=True), label=label)
     plt.legend()
     plt.show()
-    
+
 
 for label in df["label"].unique():
     subset = df[df["label"] == label]
     fig, ax = plt.subplots()
-    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label = label)
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label=label)
     plt.legend()
     plt.show()
 # --------------------------------------------------------------
@@ -51,24 +51,79 @@ mpl.rcParams["figure.dpi"] = 100
 
 category_df = df.query("label == 'squat'").query("participant == 'A'").reset_index()
 
-
+fig, ax = plt.subplots()
 category_df.groupby(["category"])["acc_y"].plot()
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 # --------------------------------------------------------------
 # Compare participants
 # --------------------------------------------------------------
+participant_df = df.query("label == 'bench'").sort_values("participant").reset_index()
 
-
+fig, ax = plt.subplots()
+participant_df.groupby(["participant"])["acc_y"].plot()
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
 # --------------------------------------------------------------
 # Plot multiple axis
 # --------------------------------------------------------------
+label = "squat"
+participant = "A"
+all_axis_df = (
+    df.query(f"label == '{label}'")
+    .query(f"participant == '{participant}'")
+    .reset_index()
+)
 
+fig, ax = plt.subplots()
+all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+ax.set_ylabel("acc_y")
+ax.set_xlabel("samples")
+plt.legend()
 
 # --------------------------------------------------------------
 # Create a loop to plot all combinations per sensor
 # --------------------------------------------------------------
+labels = df["label"].unique()
+participants = df["participant"].unique()
+
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        
+        if len(all_axis_df) > 0:
+
+            fig, ax = plt.subplots()
+            all_axis_df[["acc_x", "acc_y", "acc_z"]].plot(ax=ax)
+            ax.set_ylabel("acc_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
 
 
+for label in labels:
+    for participant in participants:
+        all_axis_df = (
+            df.query(f"label == '{label}'")
+            .query(f"participant == '{participant}'")
+            .reset_index()
+        )
+        
+        if len(all_axis_df) > 0:
+
+            fig, ax = plt.subplots()
+            all_axis_df[["gyr_x", "gyr_y", "gyr_z"]].plot(ax=ax)
+            ax.set_ylabel("gyr_y")
+            ax.set_xlabel("samples")
+            plt.title(f"{label} ({participant})".title())
+            plt.legend()
 # --------------------------------------------------------------
 # Combine plots in one figure
 # --------------------------------------------------------------
